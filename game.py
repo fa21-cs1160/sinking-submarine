77d059d4314478428951d5847c702fb03ebe5b9d
Dan Grahn <dgrahn@users.noreply.github.com>
2021-09-23 13:42:53 -0400

    Add room handler and control room look around

--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 class State:
     pass
 
+COMMAND_LOOK_AROUND = 'look around'
+
 ROOM_CONTROL = 'control'
 ROOM_ENGINE = 'engine'
 ROOM_REACTOR = 'reactor'
@@ -26,14 +28,27 @@ def handle_control_room(state, command):
     if command == 'go to reactor room':
         print(MESSAGE_CHANGE_ROOM)
         state.room = ROOM_REACTOR
+    elif command == COMMAND_LOOK_AROUND:
+        print("You see a series of panels with flashing lights. It's dim and your feet are wet. That's not a good sign.")
     else:
         print("You can't do that.")
+
+def get_room_handler(room):
+    if room == ROOM_CONTROL:
+        return handle_control_room
+    elif room == ROOM_ENGINE:
+        return handle_engine_room
+    elif room == ROOM_REACTOR:
+        return handle_reactor_room
+    else:
+        return None
 
 def main():
     state = State()
     state.room = ROOM_CONTROL
 
     while True:
+        # Print the room message
         if state.room == ROOM_CONTROL:
             print("You're in the control room.")
         elif state.room == ROOM_ENGINE:
@@ -43,20 +58,16 @@ def main():
         else:
             print(f'Unknown Room: {state.room}')
 
+        # Get the command
         print()
         command = input('What do you want to do? ').lower()
 
-        if state.room == ROOM_CONTROL:
-            handle_control_room(state, command)
-        elif state.room == ROOM_REACTOR:
-            handle_reactor_room(state, command)
-        elif state.room == ROOM_ENGINE:
-            handle_engine_room(state, command)
+        # Handle the command
+        handler = get_room_handler(state.room)
+        if handler:
+            handler(state, command)
         else:
-            print(f'Unknown Room: {state.room}')
-
-        # print(state.room)
-        # break
+            print(f'Unknown room: {state.room}')
 
     print('Game Over')
 
